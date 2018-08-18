68fa5672718e966e82efd5f56e7f2e649357aaa3
Martin Wallgren <martin@wallgren.it>
2018-08-18 15:12:09 +0200

    Remove gerrit.getChange
    We don't use it, if we need it we can add it again with proper return
    types.

--- src/gerrit.ts
+++ src/gerrit.ts
@@ -73,24 +73,6 @@ export function getReview(changeNbr: number): Promise<{ [key: string]: CommentIn
     });
 }
 
-export function getChange(change: number): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-        let base = getGerritURI();
-        if (!base) {
-            reject(Error('Gerrit host URI is not configured.'));
-            return;
-        }
-        var uri = `${base}/changes/${change}?o=ALL_REVISIONS&o=CURRENT_COMMIT`;
-        request.get(uri, function (error, _response, body) {
-            if (error) {
-                reject(Error(`Failed to load change ${change}`));
-                return;
-            }
-            resolve(toJSON(body));
-        });
-    });
-}
-
 function toJSON(body: string) {
     return JSON.parse(body.substring(")]}'".length));
 }
